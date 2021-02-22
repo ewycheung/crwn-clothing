abc2699783da8d060613f8c1d2c7d0bdd387344f
ewycheung <cheung_eddie@rogers.com>
2021-02-22 14:53:16 -0500

    added stripe integration for payment

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51INjdLCp5lCWX2Q98sQ3np18qndjG2V4iK93HAEeWQSLCgBpGkflZmDRyI9DPoYpWUhO37JQoJ0uSb37HA5LtM9u00se114pdL';
+    
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name = 'CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
+
